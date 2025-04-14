309b2eb0edc436d67b82d1b095f9f1b03ff7a784
Chamil Fonseka <Chamil.Fonseka@cambio.lk>
2025-04-14 09:48:30 +0530

    advance hover animation with single tl

--- gsap/hover/script.js
+++ gsap/hover/script.js
@@ -8,12 +8,12 @@ document.addEventListener("DOMContentLoaded", () => {
   let exitTime = 0;
 
   tl.to(bar, { scaleX: 1 })
-  .addPause();
+    .addPause();
   exitTime = tl.duration();
   tl.to(bar, { x: 100 });
 
   box.addEventListener("mouseenter", () => {
-    if(tl.time() < exitTime) {
+    if (tl.time() < exitTime) {
       tl.play();
     } else {
       tl.restart();
@@ -21,10 +21,68 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   box.addEventListener("mouseleave", () => {
-    if(tl.time() < exitTime) {
+    if (tl.time() < exitTime) {
       tl.reverse();
     } else {
       tl.play();
+    }
+  });
+
+  //-------------------------------------------------------------------------------------------------
+
+  let circlesHolder = document.querySelector(".small-circles-holder");
+  let bigCircle = document.querySelector(".big-circle");
+
+  for (var i = 0; i < 12; i++) {
+    let circle = document.createElement("div");
+    let circleWrapper = document.createElement("div");
+    circle.setAttribute("class", "small-circle");
+    circlesHolder.append(circleWrapper);
+    circleWrapper.append(circle);
+    gsap.set(circleWrapper, { rotation: i * 30 });
+    gsap.set(circle, { x: 60 });
+  }
+
+  const tl2 = gsap.timeline({paused:true})
+  let exitTime2 = 0
+
+  tl2.from(".small-circle", {
+    x: 30, duration: 0.4,
+    stagger: {
+      each: 0.05,
+      ease: "power1"
+    }
+  });
+  
+  exitTime2 = tl2.duration();
+  tl2.addPause();
+  
+  tl2.to(".small-circle", {
+    x: 80,
+    scale: 2,
+    opacity: 0,
+    duration: 0.3,
+    stagger: {
+      each: 0.08,
+      from: "end"
+    }
+  });
+  
+  tl2.to(".small-circles-holder", { rotation: 360, duration: tl2.recent().duration(), ease: "power1.in" }, "<");
+
+  bigCircle.addEventListener("mouseenter", () => {
+    if (tl2.time() < exitTime2) {
+      tl2.play();
+    } else {
+      tl2.restart();
+    }
+  });
+
+  bigCircle.addEventListener("mouseleave", () => {
+    if (tl2.time() < exitTime2) {
+      tl2.reverse();
+    } else {
+      tl2.play();
     }
   });
 });
