55eaeae169efa9b4804fa387d2795e2d4787a203
Chamil Fonseka <Chamil.Fonseka@cambio.lk>
2025-05-03 22:21:54 +0530

    Split Text - words

--- gsap/split-text/script.js
+++ gsap/split-text/script.js
@@ -1,17 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    gsap.registerPlugin(SplitText);
+  gsap.registerPlugin(SplitText);
 
-    document.fonts.ready.then(() => {
+  document.fonts.ready.then(() => {
 
-        const split1 = SplitText.create("#chars", {
-            type: "words, chars",
-            onSplit(self) {
-              gsap.from(self.chars, {
-                opacity: 0, stagger: 0.05, y: 50, ease: "back",
-              });
-            }
-          });
+    const split1 = SplitText.create("#chars", {
+      type: "words, chars",
+      onSplit(self) {
+        gsap.from(self.chars, {
+          opacity: 0, stagger: 0.05, y: 50, ease: "back",
+        });
+      }
     });
+
+    const split2 = SplitText.create("#words", {
+      type: "words",
+      onSplit(self) {
+        gsap.from(self.words, {
+          opacity: 0, stagger: { amount: 1,}, rotation: -40, scale: 2, ease: "back",
+        });
+      }
+    });
+  });
 
 });
