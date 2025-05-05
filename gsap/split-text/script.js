2c2b36fb050a9cadf8319e760547b85cbc172375
Chamil Fonseka <Chamil.Fonseka@cambio.lk>
2025-05-05 19:49:52 +0530

    Split Text - lines

--- gsap/split-text/script.js
+++ gsap/split-text/script.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
   gsap.registerPlugin(SplitText);
 
+  gsap.set(".container", {autoAlpha: 1 });
+
   document.fonts.ready.then(() => {
 
     const split1 = SplitText.create("#chars", {
@@ -18,6 +20,15 @@ document.addEventListener("DOMContentLoaded", function () {
       onSplit(self) {
         gsap.from(self.words, {
           opacity: 0, stagger: { amount: 1,}, rotation: -40, scale: 2, ease: "back",
+        });
+      }
+    });
+
+    const split3 = SplitText.create("#lines", {
+      type: "lines",
+      onSplit(self) {
+        gsap.from(self.lines, {
+          opacity:0, rotationX:-90, rotationY:-80, stagger:0.1, transformOrigin:"50% 50% -200",
         });
       }
     });
